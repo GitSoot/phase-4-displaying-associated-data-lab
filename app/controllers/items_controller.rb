d0630459d379941ca02cbfdb44308d3cf14e7627
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-03-19 15:29:56 +0300

    Assignment completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
-end
+    def index
+      items = Item.all.includes(:user)
+      render json: items.to_json(
+        only: [:id, :name, :description, :price],
+        include: { user: { only: [:id, :username, :city] } }
+      )
+    end
+  end
